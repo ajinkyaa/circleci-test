b94630bf3c3c3f5677bb3b53052e99825aaa9df6
Ajinkya Deshmukh <ajinkya@saeloun.com>
2022-02-16 22:19:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('One more deployment');
+  res.write('demo deployment');
   res.end();
 }).listen(8080, '0.0.0.0');
