7fb0dafa16179b26d77ff1b5ab61ea8a2c7fcea0
Ajinkya Deshmukh <ajinkya@saeloun.com>
2022-02-16 22:13:14 +0530

    hello deployment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('waste deployment');
+  res.write('One more deployment');
   res.end();
 }).listen(8080, '0.0.0.0');
